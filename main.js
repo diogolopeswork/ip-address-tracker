5dfa056f49690b7f49788f6ce78ad797b4473f6c
Diogo Lopes <deosl98@gmail.com>
2021-04-05 14:27:51 +0100

    init

---
+++ main.js
@@ -0,0 +1,55 @@
+let ipifyKey = 'at_LApYCnXCBsY5WE196aKK6NPnGSfZa';
+let leafletKey = 'pk.eyJ1IjoiYXJhbWluZXQiLCJhIjoiY2trODlteDlrMDV0bjJ2cTh1bjRsZm13eSJ9.6WhlUeHsibF_pfqLGxmAAA';
+let ipifyUrl = 'https://geo.ipify.org/api/v1?apiKey=' + ipifyKey + '&ipAddress=';
+
+let map = L.map('map', {zoomControl: false}).setView([48.85875893258228, 2.348786807381395], 11);
+let marker;
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: leafletKey
+}).addTo(map);
+
+searchIp();
+document.getElementById('btn').addEventListener('click', searchIp);
+document.getElementById('search-field').addEventListener('keypress', handleEnter);
+
+function handleEnter(e) {
+    if(e.keyCode == 13) {
+        e.preventDefault();
+        searchIp();
+    }
+}
+
+function searchIp() {
+    let ip = document.getElementById('search-field').value;
+    let request = new XMLHttpRequest();
+    request.open('GET', ipifyUrl + ip);
+    request.send();
+    request.onload = (e) => {
+        let response =  JSON.parse(request.responseText);
+        document.getElementById('ip').innerText = response['ip'];
+        document.getElementById('location').innerText = 
+            response['location'].city
+            + '\n'
+            + response['location'].postalCode
+            + response['location'].country;
+        document.getElementById('timezone').innerText = 'UTC' + response['location'].timezone;
+        document.getElementById('isp').innerText = response['isp'];
+
+        let lat = response['location'].lat;
+        let lng = response['location'].lng;
+        if(marker == null) {
+            marker = L.marker([lat, lng]);
+            marker.addTo(map);
+        } else {
+            marker.setLatLng([lat, lng]);
+        }
+        map.flyTo(new L.latLng(lat, lng), 15, {duration: 2});
+    }
+}
+
